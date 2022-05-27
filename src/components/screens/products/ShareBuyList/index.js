e9010cb4e45de11b17aac258cdbef47782620041
kenta.yamamomo <yamamoto7.pgming@gmail.com>
2022-05-28 00:33:43 +0900

    :sparkle: add pages

---
+++ src/components/screens/products/ShareBuyList/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  HeroSection,
+  FeatureSection,
+  ContentSectionOne,
+  ContentSectionTwo,
+  IntegrationSection,
+  CtaSection,
+  FooterSection,
+} from "./sections";
+
+const ShareBuyList = () => {
+  return (
+    <>
+      <HeroSection />
+      <FeatureSection />
+      <ContentSectionOne />
+      <ContentSectionTwo />
+      <IntegrationSection />
+      <CtaSection />
+      <FooterSection />
+    </>
+  );
+};
+
+export default ShareBuyList;
---
+++ src/components/screens/products/ShareBuyList/sections/index.js
@@ -0,0 +1,7 @@
+export { default as HeroSection } from "./Hero"
+
+export { default as FeatureSection } from "./Features"
+export { ContentSectionOne, ContentSectionTwo } from "./Content"
+export { default as IntegrationSection } from "./Integration"
+export { default as CtaSection } from "./Cta"
+export { default as FooterSection } from "./Footer"
