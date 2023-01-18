e3912591fb792cdb79e0e7cf84016b60b5ab79e2
kenta.yamamomo <yamamoto7.pgming@gmail.com>
2023-01-18 19:23:53 +0900

    add: lint setting

---
+++ src/pages/buy-list/index.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './privacy-policy.css';
+
+export default function Component() {
+    return (
+        <div className="contents_wrap">
+            <h3>開発者情報</h3>
+            <p />
+            <h3>開発者情報</h3>
+            <p />
+            <h3>開発者情報</h3>
+            <p />
+        </div>
+    );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Page from '../components/screens/page';
+import HomeScreen from '../components/screens/home';
+
+function IndexPage() {
+    return (
+        <Page>
+            <HomeScreen />
+        </Page>
+    );
+}
+
+export default IndexPage;
