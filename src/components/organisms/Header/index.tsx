f3bad06d28345e3b122e53f4526ba0484fe224bd
kenta.yamamoto <yamamoto7.pgming@gmail.com>
2023-01-27 21:23:07 +0900

    convert to ts

---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import cn from "classnames";
+import * as styles from  "./Header.module.sass";
+import getFilePath from "../../../utils/getFilePath";
+
+const Header: React.FC = () => {
+  return (
+    <header className={cn(styles.header)}>
+      <div className={cn(styles.header_wrapper)}>
+        <a className={cn(styles.logo)} href="/">
+          <img
+            src={getFilePath("logo.svg")}
+            alt="logo image for this site"
+          />
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
