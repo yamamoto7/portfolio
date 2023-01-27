d2adcf3e725bc63e111d9d774943c1969a97e7f7
kenta.yamamoto <yamamoto7.pgming@gmail.com>
2023-01-27 22:25:23 +0900

    :recycle: convert to ts

--- src/components/screens/home/Blog/index.tsx
+++ src/components/screens/home/Blog/index.tsx
@@ -3,39 +3,56 @@ import cn from "classnames";
 import * as styles from "./Blog.module.sass";
 import getFilePath from "../../../../utils/getFilePath";
 
-const posts = [
+interface Post {
+  title: string;
+  url: string;
+  img: string;
+  status?: string;
+  category: {
+    title: string;
+    color: string;
+  };
+}
+
+const posts: Post[] = [
   {
     title: "データカタログを作成してZOZOTOWNデータベース定義をまとめた話",
     url: "https://techblog.zozo.com/entry/data-catalog",
     img: "home/blog/data-catalogue.jpg",
-    // status: "badge2",
+    status: "badge2",
     category: {
       title: "Company Tech Blog",
-      color: "pink",
+      color: styles.pink,
     },
   },
   {
     title: "入門書を終えた人に捧げる、社会人のためのGit中級編",
     url: "https://qiita.com/yamamoto7/items/fe15a1e7e360b4015fae",
     img: "home/blog/git.png",
-    // status: "badge1",
+    status: "badge1",
     category: {
-      title: "",
-      color: "blue",
+      title: "hoge",
+      color: styles.blue,
     },
   },
 ];
 
-function Blog() {
+const Blog: React.FC = () => {
   return (
-    <div className={cn("section", styles.section)}>
-      <div className={cn("center", styles.center)}>
-        <h2 className={cn("title title_mb-lg")}>Recent posts</h2>
+    <div className={cn(styles.container)}>
+      <h2 className={cn(styles.title)}>Recent posts</h2>
 
-        <div className={cn(styles.list)}>
-          {posts.map((item, index) => {
-            return (
-              <a className={cn(styles.item)} href={item.url} key={index}>
+      <div className={cn(styles.list)}>
+        {posts.map((item, index) => {
+          return (
+            <a className={cn(styles.item)} href={item.url} key={index}>
+              <div
+                className={cn(styles.item_preview)}
+                style={{ backgroundImage: `url(${getFilePath(item.img)})` }}
+              ></div>
+              <div className={cn(styles.item_contents)}>
+                <h3 className={cn(styles.info)}>{item.title}</h3>
+
                 {item.status === "badge1" && (
                   <div className={cn(styles.status, styles.badge1)}>
                     Pick up
@@ -44,23 +61,16 @@ function Blog() {
                 {item.status === "badge2" && (
                   <div className={cn(styles.status, styles.badge2)}>New</div>
                 )}
-
-                <div
-                  className={styles.preview}
-                  style={{ backgroundImage: `url(${getFilePath(item.img)})` }}
-                />
-                <h3 className={styles.info}>{item.title}</h3>
-
                 <div className={cn(item.category.color, styles.category)}>
                   {item.category.title}
                 </div>
-              </a>
-            );
-          })}
-        </div>
+              </div>
+            </a>
+          );
+        })}
       </div>
     </div>
   );
-}
+};
 
 export default Blog;
